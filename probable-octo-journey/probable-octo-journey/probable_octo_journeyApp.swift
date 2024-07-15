722e9dd86c68e99f27e40cf374d8d949e1a3f70f
Peter Bishop <pjb.den@gmail.com>
2024-07-15 17:20:06 -0600

    inital project files

---
+++ probable-octo-journey/probable-octo-journey/probable_octo_journeyApp.swift
@@ -0,0 +1,32 @@
+//
+//  probable_octo_journeyApp.swift
+//  probable-octo-journey
+//
+//  Created by m1_air on 7/15/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct probable_octo_journeyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
